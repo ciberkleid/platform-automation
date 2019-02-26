a5bd6bc261b869ec18c91f472b71151b9163a233
Cora Iberkleid <ciberkleid@pivotal.io>
2019-02-26 15:07:18 -0600

    scripts to design and set parameterized pipeline

---
+++ config-designer.sh
@@ -0,0 +1,79 @@
+# Get user input
+if [ ${1} = "" ]; then
+  echo "Enter designer file [p-rabbitmq-designer.yml]: "
+  read DESIGNER
+  echo "Enter temp dir [_tmp]: "
+  read TEMP_DIR
+  #echo "Enter secrets file [~/workspace/platform-automation-private/toolsmiths-pas-p-rabbitmq.yml]: "
+  #read SECRETS
+else
+  DESIGNER=$1
+fi
+# Set variables
+DESIGNER=${DESIGNER:-p-rabbitmq-designer.yml}
+TEMP_DIR=${TEMP_DIR:-_tmp}
+#SECRETS=${SECRETS:-~/workspace/platform-automation-private/toolsmiths-pas-p-rabbitmq.yml}
+SLUG=$(om interpolate \
+      --config ${DESIGNER} \
+      --path /pivnet-product-slug)
+
+VERSION=$(om interpolate \
+      --config ${DESIGNER} \
+      --path /product_version)
+
+CONFIG_FILE=$(om interpolate \
+      --config ${DESIGNER} \
+      --path /config_file)
+
+VARS_FILES=$(om interpolate \
+      --config ${DESIGNER} \
+      --path /vars_files)
+
+OPS_FILES=$(om interpolate \
+      --config ${DESIGNER} \
+      --path /ops_files)
+
+# Clone tile-config-generator output from GitHub
+TCG=${TEMP_DIR}/vars
+
+if [ ! -f ${TCG}/${SLUG}/${VERSION}/product.yml ]; then
+    if [ -d ${TCG} ]; then rmdir ${TCG}; fi
+    mkdir -p ${TCG}
+    git clone --quiet git@github.com:DaxterM/tile-configuration.git ${TCG}
+    printf "\nCloned tile-configuration repo into ${TCG}\n\n"
+fi
+
+# Parse vars files
+vars_files_args=("")
+for vf in ${VARS_FILES}
+do
+  vars_files_args+=("--vars-file ${vf}")
+done
+
+# Parse options files
+ops_files_args=("")
+for of in ${OPS_FILES}
+do
+  ops_files_args+=("--ops-file ${of}")
+done
+
+# Create designer output file including slug, version and options files,
+# as well as the  list of additional variables for which values need to
+# be provided.
+#OUTPUT_FILE=RESULT-${DESIGNER}
+#cp ${DESIGNER} ${TEMP_DIR}/${OUTPUT_FILE}
+printf "\nChange the ops-files in your designer file until you have the desired configuration\n"
+printf "\nCopy any parameters listed below to section 3 of your designer file\n"
+printf "Follow the instructions in the header of the designer template\n\n"
+
+pushd ${TEMP_DIR} > /dev/null
+
+om interpolate --config ${CONFIG_FILE} \
+               ${ops_files_args[@]} \
+               ${vars_files_args[@]} \
+               --vars-file ../${DESIGNER} \
+               > /dev/null
+
+popd > /dev/null
+
+printf "\n"
--- fly-set-pipeline.sh
+++ fly-set-pipeline.sh
@@ -16,35 +16,22 @@ SECRETS_HOME=${SECRETS_HOME:-~/workspace/platform-automation-private}
 
 # Set fly command variables
 PIPELINE_NAME=${FOUNDATION}-${SLUG}
-PIPELINE_CONFIG_FILE=${CONFIG_HOME}/${foundation}/pipeline-parameterized.yml
-TILE_SELECTOR_FILE=${CONFIG_HOME}/${foundation}/${SLUG}-selector.yml
-SECRETS_FILE_COMMON=${SECRETS_HOME}/${foundation}-common.yml
-SECRETS_FILE_SLUG=${SECRETS_HOME}/${foundation}-${SLUG}.yml
+PIPELINE_CONFIG_FILE=${CONFIG_HOME}/${FOUNDATION}/pipeline-parameterized.yml
+SECRETS_FILE_COMMON=${SECRETS_HOME}/${FOUNDATION}/common.yml
+SECRETS_FILE=${SECRETS_HOME}/${FOUNDATION}/${SLUG}.yml
 
 # Confirm
 echo "Setting pipeline using:"
 echo "     pipeline name:   ${PIPELINE_NAME}"
 echo "     pipeline config: ${PIPELINE_CONFIG_FILE}"
-echo "     selector file:   ${TILE_SELECTOR_FILE}"
 echo "     secrets files:   ${SECRETS_FILE_COMMON}"
-echo "                      ${SECRETS_FILE_SLUG}"
+echo "                      ${SECRETS_FILE}"
 echo " Continue? [Y/N]: "
 read GO
 
 if [[ $GO == "Y" ]]; then
-
-    TEMP_FILE=_tmp/${FOUNDATION}-env.yml
-    mkdir -p "$(dirname "$TEMP_DIR")"
-    om interpolate --config ${CONFIG_HOME}/${FOUNDATION}/config/templates/env.yml \
-                   --vars-file ${SECRETS_FILE_COMMON} \
-                   > ${TEMP_FILE}
-
     fly -t w sp -p ${PIPELINE_NAME} \
                 -c ${PIPELINE_CONFIG_FILE} \
-                -l ${TILE_SELECTOR_FILE} \
                 -l ${SECRETS_FILE_COMMON} \
-                -l ${SECRETS_FILE_SLUG}
-
-    #rm ${TEMP_FILE}
-
+                -l ${SECRETS_FILE}
 fi
